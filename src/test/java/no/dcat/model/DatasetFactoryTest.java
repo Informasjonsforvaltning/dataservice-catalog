42e643aa2009ae53354c11065d2f85af03aec731
NilsOveTen <nils.ove.tendenes@acando.no>
2019-11-20 10:33:56 +0100

    Copy of registration-api

---
+++ src/test/java/no/dcat/model/DatasetFactoryTest.java
@@ -0,0 +1,26 @@
+package no.dcat.model;
+
+import no.fdk.test.testcategories.UnitTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by bjg on 02.05.2018.
+ */
+@Category(UnitTest.class)
+public class DatasetFactoryTest {
+
+    @Test
+    public void datasetCreatedWithCorrectUri() {
+        String catalogId = "12345";
+        Catalog catalog = new Catalog();
+        catalog.setId(catalogId);
+        Dataset data = new Dataset();
+        Dataset result = DatasetFactory.createDataset(catalog, data);
+
+        assertThat(result.getUri(), containsString("http://brreg.no/catalogs/" + catalogId + "/datasets/"));
+    }
+}
