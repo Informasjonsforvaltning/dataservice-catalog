968d2f3d8b7ebc8d729836836e9f7075ac6e0cdb
German Poljakov <gerpol17@gmail.com>
2020-03-16 12:42:52 +0100

    feat: #26 add http patch and delete

--- src/main/java/no/fdk/dataservicecatalog/repository/DataServiceMongoRepository.java
+++ src/main/java/no/fdk/dataservicecatalog/repository/DataServiceMongoRepository.java
@@ -2,7 +2,11 @@ package no.fdk.dataservicecatalog.repository;
 
 import no.fdk.dataservicecatalog.model.DataService;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface DataServiceMongoRepository extends ReactiveMongoRepository<DataService, String> {
-
+    Mono<DataService> findByIdAndCatalogId(String dataServiceId, String catalogId);
+    Mono<Long> deleteByIdAndCatalogId(String dataServiceId, String catalogId);
+    Flux<DataService> findAllByCatalogId(String catalogId);
 }
--- src/main/java/no/fdk/dataservicecatalog/service/DataServiceRegistrationService.java
+++ src/main/java/no/fdk/dataservicecatalog/service/DataServiceRegistrationService.java
@@ -6,10 +6,12 @@ import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiSpecification;
 import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiSpecificationSource;
 import no.fdk.dataservicecatalog.model.DataService;
 import no.fdk.dataservicecatalog.repository.DataServiceMongoRepository;
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -40,55 +42,82 @@ public class DataServiceRegistrationService {
 
     }
 
-    private DataService updateFromSpecification(String dataserviceId, ApiSpecification apiSpecification) {
-        DataService dataService = this.parseApiSpecification(apiSpecification);
-        dataService.setId(dataserviceId);
+    private DataService applyPatch(DataService dataService, Map<String, Object> partialUpdate) {
+        try {
+            BeanUtils.copyProperties(dataService, partialUpdate);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
         return dataService;
     }
 
-    public Mono<DataService> importFromSpecification(ApiSpecificationSource source) {
+    public Mono<DataService> importFromSpecification(ApiSpecificationSource source, String catalogId) {
+        Mono<ApiSpecification> apiSpecification = apiHarvesterReactiveClient.convertApiSpecification(source);
+        Mono<DataService> dataServiceMono = apiSpecification.map(this::parseApiSpecification)
+                .doOnSuccess(dataService -> log.debug("dataservice loaded from specification"))
+                .doOnError(error -> log.error("new dataservice failed mapping: {}", error.getMessage()));
+        return dataServiceMono.flatMap(dataService -> {
+            dataService.setCatalogId(catalogId);
+            return dataServiceMongoRepository.save(dataService);
+        });
+    }
+
+    public Mono<DataService> importFromSpecification(String dataServiceId, String catalogId, ApiSpecificationSource source) {
         Mono<ApiSpecification> apiSpecification = apiHarvesterReactiveClient.convertApiSpecification(source);
         Mono<DataService> dataServiceMono = apiSpecification.map(this::parseApiSpecification)
                 .doOnSuccess(dataService -> log.debug("dataservice {} loaded from specification", dataService.getId()))
-                .doOnError(error -> log.error("new dataservice failed mapping: {}", error.getMessage()));
-        return dataServiceMono.flatMap(dataServiceMongoRepository::save);
-
-    }
-
-    public Mono<DataService> importFromSpecification(String dataServiceId, ApiSpecificationSource source) {
-        Mono<ApiSpecification> apiSpecification = apiHarvesterReactiveClient.convertApiSpecification(source);
-        Mono<DataService> dataServiceMono = apiSpecification.map(spec -> this.updateFromSpecification(dataServiceId, spec))
-                .doOnSuccess(dataService -> log.debug("dataservice {} loaded from specification", dataService.getId()))
                 .doOnError(error -> log.error("dataservice with id {} failed mapping {}", dataServiceId, error.getMessage()));
-        return dataServiceMono.flatMap(dataServiceMongoRepository::save);
+        return dataServiceMono.flatMap(dataService -> {
+            dataService.setId(dataServiceId);
+            dataService.setCatalogId(catalogId);
+            return dataServiceMongoRepository.save(dataService);
+        });
     }
 
-    public Flux<DataService> getAllDataServices() {
-        var all = dataServiceMongoRepository.findAll()
+    public Flux<DataService> getAllDataServices(String catalogId) {
+        var all = dataServiceMongoRepository.findAllByCatalogId(catalogId)
                 .doOnError(error -> log.error("error retrieving all dataservices from mongo: {}", error.getMessage()));
         all.count().subscribe(count -> log.debug("found {} dataservices", count));
         return all;
     }
 
-    public Mono<DataService> create(DataService dataService) {
+    public Mono<DataService> create(DataService dataService, String catalogId) {
+        dataService.setCatalogId(catalogId);
         return dataServiceMongoRepository.save(dataService)
                 .doOnSuccess(saved -> log.debug("dataservice {} saved", saved.getId()))
                 .doOnError(error -> log.error("error saving dataservice to database: {}", error.getMessage()));
     }
 
-    public Mono<DataService> findById(String id) {
-        return dataServiceMongoRepository.findById(id)
+    public Mono<DataService> findById(String dataServiceId, String catalogId) {
+        return dataServiceMongoRepository.findByIdAndCatalogId(dataServiceId, catalogId)
                 .doOnSuccess(dataService -> {
                     if (dataService != null) {
                         log.debug("dataservice {} retrieved", dataService.getId());
                     } else {
-                        log.error("no dataservice exists with id {}", id);
+                        log.error("no dataservice with id {} exists for catalog {}", dataServiceId, catalogId);
                     }
                 })
-                .doOnError(error -> log.error("error retrieving dataservice {}: {}", id, error.getMessage()));
+                .doOnError(error -> log.error("error retrieving dataservice {}: {}", dataServiceId, error.getMessage()));
     }
 
-    public Mono<DataService> patch(Map<String, Object> partialDataService) {
-        return Mono.just(new DataService());
+    public Mono<Boolean> deleteById(String dataServiceId, String catalogId) {
+        return dataServiceMongoRepository.deleteByIdAndCatalogId(dataServiceId, catalogId)
+                .doOnError(error -> log.error("error deleting dataservice {}: {}", dataServiceId, error.getMessage()))
+                .flatMap(deletedCount -> Mono.just(deletedCount > 0))
+                .doOnSuccess(deleted -> log.debug("dataset {} deleted: {}", dataServiceId, deleted));
+    }
+
+    public Mono<DataService> patch(String dataServiceId, String catalogId, Map<String, Object> fields) {
+        return dataServiceMongoRepository.findByIdAndCatalogId(dataServiceId, catalogId)
+                .doOnSuccess(dataService -> {
+                    if (dataService != null) {
+                        log.debug("dataservice {} retrieved for patch", dataService.getId());
+                    } else {
+                        log.error("no dataservice with id {} exists for catalog {}", dataServiceId, catalogId);
+                    }
+                })
+                .doOnError(error -> log.error("error retrieving dataservice {}: {}", dataServiceId, error.getMessage()))
+                .map(dataService -> this.applyPatch(dataService, fields))
+                .flatMap(dataServiceMongoRepository::save);
     }
 }
