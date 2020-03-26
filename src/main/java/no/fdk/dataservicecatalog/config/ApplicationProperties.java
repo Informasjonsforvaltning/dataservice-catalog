0555ac26069195eee42d16f2c4c6a2eb78fb4056
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-03-26 22:16:00 +0100

    test: #25 add tests to cover new functionality

--- src/main/java/no/fdk/dataservicecatalog/config/ApplicationProperties.java
+++ src/main/java/no/fdk/dataservicecatalog/config/ApplicationProperties.java
@@ -2,10 +2,8 @@ package no.fdk.dataservicecatalog.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 @ConfigurationProperties("application")
 public class ApplicationProperties {
     private String catalogBaseUri;
---
+++ src/test/java/no/fdk/dataservicecatalog/utils/TestData.java
@@ -0,0 +1,188 @@
+package no.fdk.dataservicecatalog.utils;
+
+import no.fdk.dataservicecatalog.dto.shared.apispecification.info.Contact;
+import no.fdk.dataservicecatalog.model.DataService;
+
+import java.util.List;
+import java.util.Map;
+
+public class TestData {
+    public static List<DataService> createDataServices(String catalogId) {
+        return List.of(
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-1", catalogId))
+                        .catalogId(catalogId)
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-2", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-2-title-en")
+                                )
+                        )
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-3", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-3-title-en"),
+                                        Map.entry("nb", "id-3-title-nb")
+                                )
+                        )
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-4", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-4-title-en"),
+                                        Map.entry("nb", "id-4-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-4-description-en")
+                                )
+                        )
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-5", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-5-title-en"),
+                                        Map.entry("nb", "id-5-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-5-description-en"),
+                                        Map.entry("nb", "id-5-description-nb")
+                                )
+                        )
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-6", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-6-title-en"),
+                                        Map.entry("nb", "id-6-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-6-description-en"),
+                                        Map.entry("nb", "id-6-description-nb")
+                                )
+                        )
+                        .endpointDescription("http://endpoint-description-6")
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-7", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-7-title-en"),
+                                        Map.entry("nb", "id-7-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-7-description-en"),
+                                        Map.entry("nb", "id-7-description-nb")
+                                )
+                        )
+                        .endpointDescription("http://endpoint-description-7")
+                        .endpointUrl("http://endpoint-url-7")
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-8", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-8-title-en"),
+                                        Map.entry("nb", "id-8-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-8-description-en"),
+                                        Map.entry("nb", "id-8-description-nb")
+                                )
+                        )
+                        .endpointDescription("http://endpoint-description-8")
+                        .endpointUrl("http://endpoint-url-8")
+                        .contact(
+                                Contact
+                                        .builder()
+                                        .name("organisation-name-8")
+                                        .build()
+                        )
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-9", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-9-title-en"),
+                                        Map.entry("nb", "id-9-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-9-description-en"),
+                                        Map.entry("nb", "id-9-description-nb")
+                                )
+                        )
+                        .endpointDescription("http://endpoint-description-9")
+                        .endpointUrl("http://endpoint-url-9")
+                        .contact(
+                                Contact
+                                        .builder()
+                                        .name("organisation-name-9")
+                                        .email("email-9@email.email")
+                                        .build()
+                        )
+                        .build(),
+                DataService
+                        .builder()
+                        .id(String.format("%s/id-10", catalogId))
+                        .catalogId(catalogId)
+                        .title(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-10-title-en"),
+                                        Map.entry("nb", "id-10-title-nb")
+                                )
+                        )
+                        .description(
+                                Map.ofEntries(
+                                        Map.entry("en", "id-10-description-en"),
+                                        Map.entry("nb", "id-10-description-nb")
+                                )
+                        )
+                        .endpointDescription("http://endpoint-description-10")
+                        .endpointUrl("http://endpoint-url-10")
+                        .contact(
+                                Contact
+                                        .builder()
+                                        .name("organisation-name-10")
+                                        .email("email-10@email.email")
+                                        .url("http://organisation-url-10")
+                                        .build()
+                        )
+                        .build()
+        );
+    }
+}
