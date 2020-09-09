87173a6f09f4b4b12029b6036f0330624031fe9c
German Poljakov <gerpol17@gmail.com>
2020-09-09 10:49:13 +0200

    feat: relocate import via file from harvester

---
+++ src/main/java/no/fdk/dataservicecatalog/exceptions/ParseException.java
@@ -0,0 +1,12 @@
+package no.fdk.dataservicecatalog.exceptions;
+
+public class ParseException extends Exception {
+    public ParseException() {
+        super();
+    }
+
+    public ParseException(String message) {
+        super(message);
+    }
+}
+
--- src/main/java/no/fdk/dataservicecatalog/service/ApiHarvesterReactiveClient.java
+++ src/main/java/no/fdk/dataservicecatalog/service/ApiHarvesterReactiveClient.java
@@ -1,11 +1,11 @@
 package no.fdk.dataservicecatalog.service;
 
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import lombok.extern.slf4j.Slf4j;
 import no.fdk.dataservicecatalog.config.ApiHarvesterProperties;
-import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiHarvesterResponse;
 import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiSpecification;
 import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiSpecificationSource;
+import no.fdk.dataservicecatalog.exceptions.ParseException;
+import no.fdk.dataservicecatalog.service.parser.UniversalParser;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,19 +18,20 @@ public class ApiHarvesterReactiveClient {
     private final WebClient webClient;
 
     ApiHarvesterReactiveClient(ApiHarvesterProperties properties) {
-        webClient = WebClient.builder().defaultHeader("accept", MediaType.APPLICATION_JSON_VALUE).baseUrl(properties.getUrl()).build();
+        webClient = WebClient.builder().defaultHeader("accept", MediaType.APPLICATION_JSON_VALUE).build();
     }
 
     Mono<ApiSpecification> convertApiSpecification(ApiSpecificationSource source) {
-        var payload = JsonNodeFactory.instance.objectNode().put("url", source.getApiSpecUrl());
-
-        return webClient.post()
-                .uri("/apis/convert")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(payload)
-                .retrieve()
-                .bodyToMono(ApiHarvesterResponse.class)
-                .map(ApiHarvesterResponse::getApiSpecification);
+        var url = source.getApiSpecUrl();
+        return webClient.get().uri(url).retrieve()
+                .bodyToMono(String.class).map(spec -> {
+                    try {
+                        return new UniversalParser().parse(spec);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    return new ApiSpecification();
+                });
     }
 
 }
