3b0062f116cbd4f7e41d87d5cc77bdfaf734d121
German Poljakov <gerpol17@gmail.com>
2020-09-10 22:00:40 +0200

    test: add unit tests for import via file

---
+++ src/test/java/no/fdk/dataservicecatalog/service/parser/OpenApiV3JsonParserTest.java
@@ -0,0 +1,60 @@
+package no.fdk.dataservicecatalog.service.parser;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiSpecification;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("unit")
+public class OpenApiV3JsonParserTest {
+
+    private static String spec;
+    private static String invalidSpec;
+    private final OpenApiV3JsonParser openApiV3JsonParser = new OpenApiV3JsonParser();
+
+    @BeforeAll
+    public static void setup() throws IOException {
+        spec = IOUtils.toString(new ClassPathResource("enhetsregisteret-openapi3.json").getInputStream(), StandardCharsets.UTF_8);
+        invalidSpec = IOUtils.toString(new ClassPathResource("enhetsregisteret-openapi3-invalid-missing-title.json").getInputStream(), StandardCharsets.UTF_8);
+    }
+
+    @Test
+    public void CanParse_ShouldReturnTrue() {
+        boolean result = openApiV3JsonParser.canParse(spec);
+        assertTrue(result);
+    }
+
+    @Test
+    public void CanParse_ShouldReturnFalse() {
+        boolean result = openApiV3JsonParser.canParse(invalidSpec);
+        assertFalse(result);
+    }
+
+    @Test
+    public void ParseToOpenAPI_ShouldParse() throws Exception {
+        OpenAPI parsed = openApiV3JsonParser.parseToOpenAPI(spec);
+        assertEquals(parsed.getInfo().getTitle(), "Åpne Data fra Enhetsregisteret - API Dokumentasjon");
+    }
+
+    @Test
+    public void Parse_ShouldParse() throws Exception {
+        ApiSpecification parsed = openApiV3JsonParser.parse(spec);
+        assertEquals(parsed.getInfo().getTitle(), "Åpne Data fra Enhetsregisteret - API Dokumentasjon");
+    }
+
+    @Test
+    public void Parse_ShouldExtractFormats() throws Exception {
+        ApiSpecification parsed = openApiV3JsonParser.parse(spec);
+        assertEquals(parsed.getFormats().size(), 3);
+        assertEquals(parsed.getFormats().toArray()[0], "application/json");
+    }
+
+}
