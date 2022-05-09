936bcf2595b50ed071f287664b004593e09d7e6e
Amund Tenstad <github@amund.io>
2022-05-09 09:49:09 +0200

    Add datetime conversion module

--- src/main/java/no/fdk/dataservicecatalog/service/parser/OpenAPIToApiSpecificationConverter.java
+++ src/main/java/no/fdk/dataservicecatalog/service/parser/OpenAPIToApiSpecificationConverter.java
@@ -2,6 +2,8 @@ package no.fdk.dataservicecatalog.service.parser;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import no.fdk.dataservicecatalog.dto.shared.apispecification.ApiSpecification;
 import no.fdk.dataservicecatalog.exceptions.ParseException;
@@ -9,17 +11,14 @@ import no.fdk.dataservicecatalog.exceptions.ParseException;
 public class OpenAPIToApiSpecificationConverter {
     public static ApiSpecification convert(OpenAPI openAPI) throws ParseException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        ApiSpecification apiSpecification;
 
         try {
             String json = objectMapper.writeValueAsString(openAPI);
-            apiSpecification = objectMapper.readValue(json, ApiSpecification.class);
+            return objectMapper.readValue(json, ApiSpecification.class);
         } catch (Throwable e) {
             throw new ParseException("Error converting OpenAPI to ApiSpecification: " + e.getMessage());
         }
-
-        return apiSpecification;
     }
 }
